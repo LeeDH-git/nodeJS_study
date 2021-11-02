df2591abe60c0577de11e0a08284051f0f4d8d3b
LeeDH-git <djrl02@naver.com>
2021-11-02 16:52:18 +0900

    iniatal commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
 var app = express();
 
 // DB setting
@@ -16,11 +17,48 @@ db.on('error', function (err) {
     console.log('DB error!: ' + err);
 });
 
+
+
+// Other Setting
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// DB Schema
+var contactSchema = mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    email: { type: String },
+    phone: { type: String }
+});
+var contact = mongoose.model('contact', contactSchema);
+
+// Route
+// Home
+app.get("/", function (req, res) {
+    res.redirect('/contacts');
+});
+//contacts - index 
+app.get('/contacts', function(req, res){
+  contact.find({}, function(err, contacts){
+    if(err) return res.json(err);
+    res.render('contacts/index', {contacts:contacts});
+  });
+});
+// contacts - New 
+app.get('/contacts/new', function(req, res){
+  res.render('contacts/new');
+});
+// contacts - create 
+app.post('/contacts', function(req, res){
+  contact.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
+  });
+});
+
+// Port Setting
 var port = 3000;
 app.listen(port, function () {
     console.log('server on! http:://localhost:' + port);
 });
-
--- note.md
+++ note.md
@@ -1,1 +1,13 @@
+https://www.a-mean-blog.com/ko/blog/Node-JS-%EC%B2%AB%EA%B1%B8%EC%9D%8C/%EC%A3%BC%EC%86%8C%EB%A1%9D-%EB%A7%8C%EB%93%A4%EA%B8%B0/CRUD%EC%99%80-7-Standard-Actions
+
 몽고디비 URI : mongodb+srv://lee:lee@cluster0.we86a.mongodb.net/TestDB?retryWrites=true&w=majority
+
+npm install --g nodemon
+
+index : GET
+show : GET
+new : GET
+create : POS
+edit : GET
+update : PUT
+destroy : DELETE
