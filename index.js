5fa0df486a018b33aa52df6298a693fed5681c51
LeeDH-git <djrl02@naver.com>
2021-11-04 16:01:23 +0900

    dd

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var express = require('express')
-var mongoose = require('mongoose')
-var bodyParser = require('body-parser')
-var app = express();
+import express from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import methodOverride from 'method-override'
+
+let app = express();
 
 // DB setting
 mongoose.connect(process.env.MONGO_DB); // process.env 오브젝트는 환경변수들을 가지고 있는 객체(node.js에서 기본으로 제공)
-var db = mongoose.connection;
-
+let db = mongoose.connection;
 // once(event,listener) 메소드 = 이벤트 리스너 함수가 한번이라도 실행하고 나면 자동으로 제거 되므로 이벤트를 딱 한번만 받아서 처리 할 수 있음.
 db.once('open', function () {
     console.log('DB connected');
@@ -17,48 +18,58 @@ db.on('error', function (err) {
     console.log('DB error!: ' + err);
 });
 
-
-
 // Other Setting
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 // DB Schema
-var contactSchema = mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-    email: { type: String },
-    phone: { type: String }
-});
-var contact = mongoose.model('contact', contactSchema);
+// let contactSchema = mongoose.Schema({
+//     name: { type: String, required: true, unique: true },
+//     email: { type: String },
+//     phone: { type: String }
+// });
+// let contact = mongoose.model('contact', contactSchema);
 
 // Route
+app.use("/", require('./route/home'))
+
 // Home
-app.get("/", function (req, res) {
-    res.redirect('/contacts');
+router.get('/', function(req, res){
+  res.render('home/welcome');
 });
-//contacts - index 
-app.get('/contacts', function(req, res){
-  contact.find({}, function(err, contacts){
-    if(err) return res.json(err);
-    res.render('contacts/index', {contacts:contacts});
-  });
-});
-// contacts - New 
-app.get('/contacts/new', function(req, res){
-  res.render('contacts/new');
-});
-// contacts - create 
-app.post('/contacts', function(req, res){
-  contact.create(req.body, function(err, contact){
-    if(err) return res.json(err);
-    res.redirect('/contacts');
-  });
+router.get('/about', function(req, res){
+  res.render('home/about');
 });
 
 // Port Setting
-var port = 3000;
+let port = 3000;
 app.listen(port, function () {
     console.log('server on! http:://localhost:' + port);
 });
+
+
+// // Home
+// app.get("/", function (req, res) {
+//     res.redirect('/contacts');
+// });
+// //contacts - index 
+// app.get('/contacts', function(req, res){
+//   contact.find({}, function(err, contacts){
+//     if(err) return res.json(err);
+//     res.render('contacts/index', {contacts:contacts});
+//   });
+// });
+// // contacts - New 
+// app.get('/contacts/new', function(req, res){
+//   res.render('contacts/new');
+// });
+// // contacts - create 
+// app.post('/contacts', function(req, res){
+//   contact.create(req.body, function(err, contact){
+//     if(err) return res.json(err);
+//     res.redirect('/contacts');
+//   });
+// });
--- note.md
+++ note.md
@@ -1,8 +1,10 @@
 https://www.a-mean-blog.com/ko/blog/Node-JS-%EC%B2%AB%EA%B1%B8%EC%9D%8C/%EC%A3%BC%EC%86%8C%EB%A1%9D-%EB%A7%8C%EB%93%A4%EA%B8%B0/CRUD%EC%99%80-7-Standard-Actions
+https://www.a-mean-blog.com/ko/blog/Node-JS-%EC%B2%AB%EA%B1%B8%EC%9D%8C/%EA%B2%8C%EC%8B%9C%ED%8C%90-%EB%A7%8C%EB%93%A4%EA%B8%B0
 
 몽고디비 URI : mongodb+srv://lee:lee@cluster0.we86a.mongodb.net/TestDB?retryWrites=true&w=majority
 
 npm install --g nodemon
+npm install --s method-override
 
 index : GET
 show : GET
