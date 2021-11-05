0b34a0d48a39223c54201bd51c6d7a5767ce45ef
LeeDH-git <djrl02@naver.com>
2021-11-05 16:20:13 +0900

    dd

--- route/posts.js
+++ route/posts.js
@@ -3,6 +3,12 @@ let router = express.Router();
 let modelPost = require("../model/posts");
 
 // index
+// res.render는 ejs파일을 html로 만들어 client(브라우저)로 return하는 함수
+/** 모델.find(검색조건, callback_함수)
+ * 모델.find의 검색조건은 Object 형태로 전달
+ * 빈 Object({})를 전달하는 경우(=검색조건 없음)
+ * DB에서 해당 모델의 모든 data를 return
+ * */
 router.get("/", function (req, res) {
   modelPost
     .find({})
@@ -19,6 +25,9 @@ router.get("/new", function (req, res) {
 });
 
 // create
+/** 모델.create은 DB에 data를 생성하는 함수
+ *  첫번째 parameter로 error를 받고 두번째 parameter로 생성된 data를 받음
+ */
 router.post("/", function (req, res) {
   modelPost.create(req.body, function (err, post) {
     if (err) return res.json(err);
